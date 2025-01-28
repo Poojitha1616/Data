a26946b8c7c677970bf5105034184bb5b150a01a
Poojitha1616 <gudemsaipoojitha2@gmail.com>
2025-01-28 22:25:33 +0530

    Add files via upload
    this program displays the all basic datatypes.

---
+++ Data.java
@@ -0,0 +1,19 @@
+//•	Write a program to demonstrate the usage of all basic data types in Java.
+public class Data {
+    public static void main(String args[]) {
+    char a = 'G';
+    int i = 89;
+    byte b = 40;
+    short s = 56;
+    double d = 4.355453532;
+    float f = 4.7333434f;
+    long l = 12121;
+    System.out.println("char: " + a);
+    System.out.println("integer: " + i);
+    System.out.println("byte: " + b);
+    System.out.println("short: " + s);
+    System.out.println("float: " + f);
+    System.out.println("double: " + d);
+    System.out.println("long: " + l);
+    }
+}
